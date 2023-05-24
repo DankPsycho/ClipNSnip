2b85348a9e3370367b007019f29fe86424966787
Sizi <john7cisors@gmail.com>
2023-05-24 22:30:58 +0200

    Service Button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Find the button and set the OnClickListener using lambda expression
-        Button reservationButton = findViewById(R.id.reservation_button);
+        Button reservationButton = findViewById(R.id.button_reservation);
         reservationButton.setOnClickListener(v -> {
             // Handle button click and navigate to the reservation screen
             Intent intent = new Intent(MainActivity.this, ReservationActivity.class);
--- app/src/main/java/com/example/myapplication/ReservationActivity.java
+++ app/src/main/java/com/example/myapplication/ReservationActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ReservationActivity extends AppCompatActivity {
 
@@ -11,7 +13,13 @@ public class ReservationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
 
-        // Add your code for handling service selection and making a reservation
+        // Find the button and set the OnClickListener using lambda expression
+        Button reservationButton = findViewById(R.id.button_select_services);
+        reservationButton.setOnClickListener(v -> {
+            // Handle button click and navigate to the reservation screen
+            Intent intent = new Intent(ReservationActivity.this, ConfirmationActivity.class);
+            startActivity(intent);
+        });
 
     }
 
