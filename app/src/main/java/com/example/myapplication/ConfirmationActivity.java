edd988602e5deb282791018432b1da6808c01cdb
Sizi <john7cisors@gmail.com>
2023-05-25 22:09:41 +0200

    Database working made radio buttons for time calendar finally populates database bugfixes

--- app/src/main/java/com/example/myapplication/ConfirmationActivity.java
+++ app/src/main/java/com/example/myapplication/ConfirmationActivity.java
@@ -1,14 +1,104 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ConfirmationActivity extends AppCompatActivity {
+
+    private DBHelper dbHelper;
+    private SQLiteDatabase database;
+    private RadioGroup shiftsRadioGroup;
+    private int lastCheckedId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+
+        // Initialize the DBHelper
+        dbHelper = new DBHelper(this);
+        database = dbHelper.getWritableDatabase();
+
+        // Find the "Confirm Reservation" button and set the OnClickListener
+        Button confirmationButton = findViewById(R.id.confirmation_button);
+        confirmationButton.setOnClickListener(v -> {
+            // Get the selected date from the calendar view
+            CalendarView calendarView = findViewById(R.id.calendarView);
+            long selectedDateInMillis = calendarView.getDate();
+            // Convert the selected date to a desired format if needed
+            String selectedDate = formatDate(selectedDateInMillis);
+
+            // Get the selected time from the selected radio button
+            String selectedTime = getSelectedTime(shiftsRadioGroup);
+
+            // Insert the reservation into the SQLite table
+            ContentValues values = new ContentValues();
+            values.put(DBHelper.getColumnDate(), selectedDate);
+            values.put(DBHelper.getColumnTime(), selectedTime);
+
+            database.insert(DBHelper.getTableReservation(), null, values);
+            dbHelper.addReservation(selectedDate, selectedTime);
+
+            // Show a toast message for confirmation
+            Toast.makeText(this, "Reservation confirmed", Toast.LENGTH_SHORT).show();
+        });
+
+        // Find the shifts radio group and assign it to the shiftsRadioGroup variable
+        shiftsRadioGroup = findViewById(R.id.shiftsRadioGroup);
+        lastCheckedId = shiftsRadioGroup.getCheckedRadioButtonId();
+
+        // Set an OnCheckedChangeListener on the shiftsRadioGroup
+        shiftsRadioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            RadioButton selectedRadioButton = findViewById(checkedId);
+            selectedRadioButton.setTextColor(ContextCompat.getColor(this, android.R.color.black));
+
+            if (lastCheckedId != -1 && lastCheckedId != checkedId) {
+                RadioButton lastSelectedRadioButton = findViewById(lastCheckedId);
+                lastSelectedRadioButton.setTextColor(ContextCompat.getColor(this, android.R.color.white));
+            }
+
+            lastCheckedId = checkedId;
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Close the database when the activity is destroyed
+        if (database != null && database.isOpen()) {
+            database.close();
+        }
+    }
+
+    private String formatDate(long dateInMillis) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        return sdf.format(new Date(dateInMillis));
+    }
+
+    private String getSelectedTime(RadioGroup shiftsGroup) {
+        StringBuilder selectedTime = new StringBuilder();
+        int selectedRadioButtonId = shiftsGroup.getCheckedRadioButtonId();
+        if (selectedRadioButtonId != -1) {
+            RadioButton radioButton = findViewById(selectedRadioButtonId);
+            selectedTime.append(radioButton.getText()).append(", ");
+        }
+        // Remove the trailing comma and space
+        if (selectedTime.length() > 0) {
+            selectedTime.delete(selectedTime.length() - 2, selectedTime.length());
+        }
+        return selectedTime.toString();
     }
 }
---
+++ app/src/main/java/com/example/myapplication/DBHelper.java
@@ -0,0 +1,59 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.ContentValues;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "reservation.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Table name and column names
+    private static final String TABLE_RESERVATION = "reservation";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_DATE = "date";
+    private static final String COLUMN_TIME = "time";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create the reservation table
+        String createTableQuery = "CREATE TABLE " + TABLE_RESERVATION + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_DATE + " TEXT, "
+                + COLUMN_TIME + " TEXT)";
+        db.execSQL(createTableQuery);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop the existing table and recreate it
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_RESERVATION);
+        onCreate(db);
+    }
+
+    public void addReservation(String date, String time) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_DATE, date);
+        values.put(COLUMN_TIME, time);
+        db.insert(TABLE_RESERVATION, null, values);
+        db.close();
+    }
+    public static String getColumnDate() {
+        return COLUMN_DATE;
+    }
+    public static String getColumnTime() {
+        return COLUMN_TIME;
+    }
+    public static String getTableReservation() {
+        return TABLE_RESERVATION;
+    }
+    // Add other helper methods as needed for querying or manipulating the database
+
+}
